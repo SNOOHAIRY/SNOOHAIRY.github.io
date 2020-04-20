cdd17b13be075325f9fade27c0a4f5a6939f6406
HP1020 <2088797670@qq.com>
2020-04-20 11:08:05 +0800

    Site updated: 2020-04-20 11:08:05

---
+++ js/titletitle.js
@@ -0,0 +1,17 @@
+<!--浏览器标题-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'see ya o_O ....';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'Welcome back╮(╯▽╰)╭ ' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
